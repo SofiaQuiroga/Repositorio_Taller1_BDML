58c3b504da7f6096322c5866962ef3dd198a2a9c
María Carolina Gutiérrez <m.gutierreza@uniandes.edu.co>
2023-02-10 13:55:22 -0500

    Update Taller1.R
    Limpiar un poco más la base. Generar las variables del punto 2 y eliminar valores indefinidos del logaritmo de los salarios.

--- Taller 1/Taller1.R
+++ Taller 1/Taller1.R
@@ -1,9 +1,19 @@
-##Taller 1
+##TALLER 1
+
+### Clean the workspace -----------------------------------------------------
+rm(list=ls())
+cat("\014")
+local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org"; options(repos=r)}) #set repo
+
 
 ##Punto 2
 
-require(pacman)
-p_load(tidyverse,rvest) ##cargamos los paquetes.
+###Instalamos los paquetes -----------------------------------------------------
+
+require("pacman")
+p_load("tidyverse","rvest", "stargazer") ##cargamos los paquetes.
+
+###Hacemos el web scraping para acceder a los datos ----------------------------
 
 #Tabla 1
 Link1<-read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html")
@@ -64,7 +74,44 @@ Tabla10<-html_table(Link10)
 Tabla10<-as.data.frame(Tabla10)
 BaseA<-merge(BaseA,Tabla10, all = TRUE)
 
-##Limpiar la Base
-BaseB<-BaseA[BaseA$age>=18, ] #Base con personas mayores a 18 a�os
+##Limpiar la Base -----------------------------------------------------
+
+#LUIS CARLOS
+BaseB<-BaseA[BaseA$age>=18, ] #Base con personas mayores a 18 años
+
 #variables principales
 BaseC<-select(BaseB,age,clase,college,hoursWorkUsual,informal,p6090,sex,totalHoursWorked,y_salary_m,y_salary_m_hu)
+
+
+#SOFÍA 
+#Limpieza base de datos
+datos <- select(BaseA, sex, estrato1, age, p6210s1, p6426, p6090, depto, ingtot, clase)
+datos <- na.omit(datos)
+datos <- datos %>% filter(age>17)
+summary(datos)
+
+
+#Cambiando nombre de las variables
+datos <- datos %>% rename(estrato = estrato1,
+                          edad = age, 
+                          maxEducativo = p6210, 
+                          tiempoTrabajo = p6426,
+                          salud = p6090, 
+                          salario = ingtot)
+summary(datos)
+
+##Punto 3
+###Genero la variable independiente ----------------------------
+
+datos<- datos %>% mutate(age_2=age^2, 
+                     log_income=log(ingtot) )
+
+###La regresión ----------------------------
+
+nuevos_datos <- datos                               # Duplicate data
+nuevos_datos[is.na(nuevos_datos) | nuevos_datos == "Inf"] <- NA
+
+nuevos_datos[is.na(nuevos_datos) | nuevos_datos=="-Inf"]= NA
+
+mod1<-lm(log_income~age+age_2, data = nuevos_datos)
+stargazer(mod1,type="text")
